0e787eef5075cad4c14e7341fd1fa5a6169fad8a
Chris Jewell <chrisjohnjewell@gmail.com>
2018-03-20 21:07:24 +1100

    Add basic Artist seeder

--- apps/yff_backend/priv/repo/seeds.exs
+++ apps/yff_backend/priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.show
+
+defmodule YFFBackend.DatabaseSeeder do
+  alias YFFBackend.{ Program.Artist, Program }
+
+  @artist_file Path.join(:code.priv_dir(:yff_backend), "repo/seeds/artists.json")
+
+  def insert_all!() do
+    artists() |> Enum.each(&insert_artist/1)
+  end
+
+  def insert_artist(%{"name" => name} = artist) do
+    case Program.get_artist_by_name(name) do
+      nil ->  Program.create_artist(artist)
+      %Artist{} = artist -> {:ok, artist}
+    end
+  end
+
+  def artists() do
+    with {:ok, body} <- File.read(@artist_file),
+         {:ok, %{"artists" => artists}} <- Poison.decode(body)
+    do
+      artists
+    else
+      error -> error
+    end
+  end
+end
+
+YFFBackend.DatabaseSeeder.insert_all!()
