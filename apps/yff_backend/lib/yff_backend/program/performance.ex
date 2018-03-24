104ba52baf729316d071b49123ef62306290a059
Chris Jewell <chrisjohnjewell@gmail.com>
2018-03-24 14:49:03 +1100

    Add day enum to Performance

--- apps/yff_backend/lib/yff_backend/program/performance.ex
+++ apps/yff_backend/lib/yff_backend/program/performance.ex
@@ -9,6 +9,7 @@ defmodule YFFBackend.Program.Performance do
   @foreign_key_type :binary_id
   schema "performances" do
     field :time, :utc_datetime
+    field :day, DayEnum
     belongs_to :artist, Artist
 
     timestamps()
@@ -17,7 +18,7 @@ defmodule YFFBackend.Program.Performance do
   @doc false
   def changeset(performance, attrs) do
     performance
-    |> cast(attrs, [:artist_id, :time])
+    |> cast(attrs, [:artist_id, :time, :day])
     |> foreign_key_constraint(:artist)
     |> validate_required([:time, :artist_id])
   end
--- apps/yff_backend/priv/repo/migrations/20180324013442_create_performances.exs
+++ apps/yff_backend/priv/repo/migrations/20180324013442_create_performances.exs
@@ -1,13 +1,20 @@
 defmodule YFFBackend.Repo.Migrations.CreatePerformances do
   use Ecto.Migration
 
-  def change do
+  def up do
+    DayEnum.create_type()
     create table(:performances, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :artist_id, references(:artists, type: :binary_id), null: false
       add :time, :utc_datetime, null: false
+      add :day, :day
 
       timestamps()
     end
+  end
+
+  def down do
+    drop table(:performances)
+    DayEnum.drop_type()
   end
 end
